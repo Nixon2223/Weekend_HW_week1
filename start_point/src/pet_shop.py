3a7ea62d43faf5fee7c6eb852b8d8ab54198e8ac
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-19 13:52:15 +0000

    7 tests passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -13,4 +13,5 @@ def get_pets_sold(pet_shop):
 def increase_pets_sold(pet_shop, amount):
     pet_shop["admin"]["pets_sold"] += amount
 
-
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
