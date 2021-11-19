71aef94db4bfcc2afa5ecd45b6d26443246dca29
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-19 14:30:58 +0000

    14 tests passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -34,6 +34,9 @@ def remove_pet_by_name(pet_shop, name):
 def add_pet_to_stock(pet_shop, new_pet):
     return pet_shop["pets"].append(new_pet)
 
+def get_customer_cash(customer):
+    return customer["cash"]
+
 
     
 
