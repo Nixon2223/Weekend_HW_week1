a544729356b21d7ae6abe02984961759e75c0e50
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-19 14:14:01 +0000

    10 tests passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -23,3 +23,9 @@ def get_pets_by_breed(pet_shop, breed):
             l.append(pet["name"])
     return l
 
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+
+
